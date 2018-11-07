c0fdfa300d0dd83353b92feee74c81bc6a472884
Katherine Hansen <kahansen@chapman.edu>
2018-11-07 11:48:44 -0800

    Adds numbers.sh

--- numbers.sh
+++ numbers.sh
@@ -1,1 +1,17 @@
-
+#! /bin/bash
+# numbers.sh
+#Katherine Hansen
+echo "Give me a positive number"
+read number
+N=1
+while [ $N -le $number ]
+	do
+		if [ $((N%2)) -eq 0 ]
+		then
+			echo $N " Even"
+		else
+			echo $N " Odd"
+		fi
+		N=$((N+1))
+	done
+echo "Thank you"
